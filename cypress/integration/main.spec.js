5398ea34f67a8e37533c7cba936f8c55ab923980
Marty Henderson <10hendersonm@gmail.com>
2018-10-12 14:03:47 -0500

    added additional files

---
+++ cypress/integration/main.spec.js
@@ -0,0 +1,5 @@
+describe('Main', () => {
+  it('Renders', () => {
+    cy.visit('http://localhost:3000')
+  })
+})
---
+++ src/App/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  render () {
+    const style = {
+      container: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+        padding: 8,
+      },
+    }
+
+    return (
+      <div style={style.container}>
+        
+      </div>
+    )
+  }
+}
+
+export default App
